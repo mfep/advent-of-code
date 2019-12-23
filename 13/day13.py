fd3ad557e2aeb06b54cc228799ac0fe3200105e6
Lőrinc Serfőző <serfozo.lorinc@gmail.com>
2019-12-23 15:16:50 +0100

    day13 part two

--- 13/day13.py
+++ 13/day13.py
@@ -1,3 +1,4 @@
+from os import system
 from collections import defaultdict
 
 class Cpu():
@@ -40,6 +41,7 @@ class Cpu():
         if not self.running:
             raise Exception('program already exited')
 
+        output = []
         while True:
             opspec = self.program[self.iptr]
             opcode = int(str(opspec)[-2:])
@@ -52,12 +54,15 @@ class Cpu():
                 memset(2, opspec, val)
                 self.iptr += 4
             elif opcode == 3: # INP
+                if input_value is None:
+                    return output
                 memset(0, opspec, input_value)
+                input_value = None
                 self.iptr += 2
             elif opcode == 4: # OUT
                 val = memget(0, opspec)
+                output.append(val)
                 self.iptr += 2
-                return val
             elif opcode == 5: # JMP-T
                 load1 = memget(0, opspec)
                 if load1 == 0:
@@ -93,13 +98,51 @@ class Cpu():
                 break
             else:
                 raise Exception('unexpected instruction', opcode)
-        return None
+        return output
+
+
+def init_display(data):
+    xyval = list(zip(*[iter(data)] * 3))
+    max_x = max(x for x,_,_ in xyval)
+    max_y = max(y for _,y,_ in xyval)
+    return [[' ' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+
+def display(data, display_data):
+    for x, y, obj_id in xyval:
+        if x == -1 and y == 0:
+            print('score', obj_id)
+        elif obj_id == 0:
+            out_str = ' '
+        elif obj_id == 1:
+            out_str = '█'
+        elif obj_id == 2:
+            out_str = '░'
+        elif obj_id == 3:
+            out_str = '▔'
+        elif obj_id == 4:
+            out_str = '○'
+        else:
+            raise Exception('unexpected symbol')
+        display_data[y][x] = out_str
 
 with open('day13.txt') as f:
     program = [int(char) for char in f.readline().split(',')]
-
 cpu = Cpu(program)
-output = []
-while cpu.running:
-    output.append(cpu.run(None))
+output = cpu.run(None)
 print('part one', sum(1 for idx,elem in enumerate(output) if idx % 3 == 2 and elem == 2))
+display_data = init_display(output)
+program[0] = 2
+cpu = Cpu(program)
+input_dir = 0
+while cpu.running:
+    output = cpu.run(input_dir)
+    xyval = list(zip(*[iter(output)] * 3))
+    ball = [x for x,_,obj_id in xyval if obj_id == 4]
+    if len(ball):
+        ball_x = ball[0]
+    paddle = [x for x,_,obj_id in xyval if obj_id == 3]
+    if len(paddle):
+        paddle_x = paddle[0]
+    input_dir = 0 if ball_x == paddle_x else -1 if ball_x < paddle_x else 1
+    display(xyval, display_data)
+    print('\n'.join(''.join(row) for row in display_data))
